49f0093db6a0db7b56aaa4f01347de154a238276
Charles Liu <che-yuan.liu@mail.mcgill.ca>
2014-06-11 16:44:22 -0400

    test window with freeglut and glew

--- triangle/triangle.cpp
+++ triangle/triangle.cpp
@@ -39,14 +39,16 @@ int main(void)
         glMatrixMode(GL_MODELVIEW);
         glLoadIdentity();
         glRotatef((float) glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
+        /*Old OpenGL*/    
         glBegin(GL_TRIANGLES);
-        glColor3f(1.f, 0.f, 0.f);
-        glVertex3f(-0.6f, -0.4f, 0.f);
-        glColor3f(0.f, 1.f, 0.f);
-        glVertex3f(0.6f, -0.4f, 0.f);
-        glColor3f(0.f, 0.f, 1.f);
-        glVertex3f(0.f, 0.6f, 0.f);
+            glColor3f(1.f, 0.f, 0.f);
+            glVertex3f(-0.6f, -0.4f, 0.f);
+            glColor3f(0.f, 1.f, 0.f);
+            glVertex3f(0.6f, -0.4f, 0.f);
+            glColor3f(0.f, 0.f, 1.f);
+            glVertex3f(0.f, 0.6f, 0.f);
         glEnd();
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
