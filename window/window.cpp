ffbf3770588301d72540a3cb4df5bd4298532db3
Charles Liu <che-yuan.liu@mail.mcgill.ca>
2014-06-11 16:47:56 -0400

    comment

--- window/window.cpp
+++ window/window.cpp
@@ -1,9 +1,9 @@
-#include <GL/glew.h> // Include the GLEW header file  
-#include <GL/glut.h> // Include the GLUT header file  
+#include <GL/glew.h>   
+#include <GL/glut.h> 
   
 void display (void) {  
-	glClearColor(1.0f, 0.0f, 1.0f, 1.0f); // Clear the background of our window to red  
-	glClear(GL_COLOR_BUFFER_BIT); //Clear the colour buffer (more buffers later on)  
+	glClearColor(1.0f, 0.0f, 1.0f, 1.0f); // Choose color for clearing background (RGBA)
+	glClear(GL_COLOR_BUFFER_BIT); //Clear background with color chosen  
 	glLoadIdentity(); // Load the Identity Matrix to reset our drawing locations  
 	  
 	glFlush(); // Flush the OpenGL buffers to the window  
@@ -11,8 +11,8 @@ void display (void) {
   
 int main (int argc, char **argv) {  
 
-	glutInit(&argc, argv); // Initialize GLUT  
-	glutInitDisplayMode (GLUT_SINGLE); // Set up a basic display buffer (only single buffered for now)  
+	glutInit(&argc, argv);   
+	glutInitDisplayMode (GLUT_SINGLE); // Set basic display mode to single buffer 
 	glutInitWindowSize (500, 500); // Set the width and height of the window  
 	glutInitWindowPosition (100, 100); // Set the position of the window  
 	
@@ -20,5 +20,7 @@ int main (int argc, char **argv) {
 	  
 	glutDisplayFunc(display); // Tell GLUT to use the method "display" for rendering  
 	  
-	glutMainLoop(); // Enter GLUT's main loop  
+	glutMainLoop(); // Enter GLUT's main loop, which is repeatedly displaying. Not interesting, yet.
 } 
+
+//Modeled after http://www.swiftless.com/tutorials/opengl/window.html
